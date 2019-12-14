c24082ffc23ad0401b1bf7f323f97266e88a7609
emperwang <544094478@qq.com>
2019-12-14 16:28:31 +0800

    add comment

--- args/custumArgaParse.py
+++ args/custumArgaParse.py
@@ -6,6 +6,18 @@ import argparse
 
 """
 解析参数的函数
+name/flags: 参数的名字
+action: 遇到参数时的动作，默认值为 store
+narsg: 参数的个数，可以是具体的数组，或者是 "+" 或 "*"。其中 + 表示1或多个参数，  *表示0或多个参数
+const action 和 nargs: 需要的常量值
+default: 不指定参数时的默认值
+type: 参数的类型
+choices: 参数允许的值
+required: 可选，参数是否是必需的
+help: 参数帮助信息
+metavar: 在usage说明中参数名字
+dest: 解析后的参数名称
+
 """
 def __argparse():
     parse = argparse.ArgumentParser(description='A cmd param client')
