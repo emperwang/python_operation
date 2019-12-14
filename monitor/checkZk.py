8e3c3d46aca3ea1251197fb03b0e3f8180dee6cb
emperwang <544094478@qq.com>
2019-12-14 20:18:22 +0800

    check zk

--- monitor/checkZk.py
+++ monitor/checkZk.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # coding=utf-8
 import argparse
-import json
 import os
 
 BASE = os.getcwd()
@@ -16,20 +15,29 @@ def check(ip, usr, cmd):
     print("checking host:", ip)
     c = "ssh "+usr+"@"+ip+ "  'source /etc/profile;"+cmd+"  status'"
     result = os.popen(c)
-    for i in result.readlines():
-        print(i)
+    lines = result.readlines()
+    resStr = str(lines)
+    if "follower" in resStr or "leader" in resStr or "observer" in resStr:
+        print(ip, resStr)
+        return True
 
 def main():
     parser = __paramget()
     cmds = dict(cmd=parser.cmd, hosts=parser.host)
-    print(cmds)
+    numhosts = 0
+    succhost = 0
     if "," in cmds['hosts']:
         for h in cmds['hosts'].split(","):
-            check(h, "root", cmds['cmd'])
+            numhosts += 1
+            if check(h, "root", cmds['cmd']):
+                succhost += 1
     else:
-        print("host:", cmds['hosts'])
-        check(cmds['hosts'], "root", cmds['cmd'])
+        if check(cmds['hosts'], "root", cmds['cmd']):
+            succhost += 1
+    if succhost > numhosts/2 :
+        print("zk cluster running")
 
+# python checkZk.py -c /mnt/zookeeper-3.4.5-cdh5.12.0/bin/zkServer.sh  -i name1,node1,node2 2>/dev/null
 
 if __name__ == '__main__':
     main()
