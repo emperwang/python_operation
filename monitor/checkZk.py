85abd2c037380147e141f4229fccfb81917ab1bb
emperwang <544094478@qq.com>
2019-12-14 18:20:08 +0800

    fix desc

--- monitor/checkZk.py
+++ monitor/checkZk.py
@@ -9,7 +9,7 @@ BASE = os.getcwd()
 def __paramget():
     parser = argparse.ArgumentParser(description='zookeeper check script.')
     parser.add_argument('-c', '--cmd', action='store', dest='cmd', required=True, help='cmd to exec.')
-    parser.add_argument('-i', '--host', action='store', dest='host', required=True, help='hosts to check.(ip:port,ip2:port)')
+    parser.add_argument('-i', '--host', action='store', dest='host', required=True, help='hosts to check.(ip,ip2)')
     return  parser.parse_args()
 
 def check(ip, usr, cmd):
