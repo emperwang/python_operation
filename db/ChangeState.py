d6cee09a775514c2858c9de44edf1d099a80b340
kai.wang <kai.l.wang@ericsson.com>
2020-10-28 15:24:06 +0800

    fix script

--- db/ChangeState.py
+++ db/ChangeState.py
@@ -19,20 +19,20 @@ def amCollectorHostInfoOperate():
     cur.execute("SELECT collector_id,node_state,state from am_collector_host_info")
     rows = cur.fetchall()
     for row in rows:
-        print "Deal collector_id:", row[0], ",node_state:", row[1], ",state:", row[2]
+        print("Deal collector_id:", row[0], ",node_state:", row[1], ",state:", row[2])
 
         if row[1] == 'MASTER':
             cur.execute("update am_collector_host_info set node_state='%s', state='%s' where collector_id='%s'"%('SLAVE','standby',row[0]))
-            print "Change node_state to SLAVE, state to standby"
+            print("Change node_state to SLAVE, state to standby")
         elif row[1] == 'SLAVE':
             cur.execute("update am_collector_host_info set node_state='%s', state='%s' where collector_id='%s'"%('MASTER','standby',row[0]))
-            print "Change node_state to MASTER, state to standby"
-        print "Total number of rows operate :", cur.rowcount
+            print("Change node_state to MASTER, state to standby")
+        print("Total number of rows operate :", cur.rowcount)
 
     CONN.commit()
     cur.close()
     CONN.close()
-    print "Operation done successfully";
+    print("Operation done successfully")
 
 def main():
     global CONN, DBHOST, ConfigFile
@@ -41,10 +41,10 @@ def main():
             for line in f:
                 if line.find('#') == -1 and line.find('jdbc:postgresql') !=-1:
                     DBHOST = line[line.find('sql://')+len('sql://'):line.find(':5432/')]
-                    print "DBHOST:",DBHOST
+                    print("DBHOST:",DBHOST)
 
     CONN = psycopg2.connect(database=DATABASE, user=DBUSER, password=DBPASSWORD, host=DBHOST, port=DBPORT)
-    print "Connect database successfully."
+    print("Connect database successfully.")
 
     amCollectorHostInfoOperate()
 
