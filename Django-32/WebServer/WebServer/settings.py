1b6fafe5d2eece8b1bbf4ff2ab85eac1b38c3942
emperWang <544094478@qq.com>
2022-09-17 08:04:29 +0800

    django

--- Django-32/WebServer/WebServer/settings.py
+++ Django-32/WebServer/WebServer/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # 静态资源的 url路径
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
